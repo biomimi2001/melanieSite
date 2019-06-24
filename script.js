c9bf3fd76a5d7b53817309aa7140622281a0b4bc
Mimart <mimart.yasmina@gmail.com>
2019-06-24 16:27:48 +0200

    first

---
+++ script.js
@@ -0,0 +1,59 @@
+ M.AutoInit();
+
+//SCROLL REVEAL
+
+ScrollReveal().reveal('h1', {
+    duration: 3000,
+    scale: 0.8,
+    reset: true
+});
+
+ScrollReveal().reveal('h2', {
+    duration: 4000,
+    scale: 0.6,
+    reset: true
+});
+
+ScrollReveal().reveal('.ser1', {
+    duration: 3500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+ScrollReveal().reveal('.ser2', {
+    duration: 2500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+ScrollReveal().reveal('.ser3', {
+    duration: 1500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+
+ScrollReveal().reveal('.robe', {
+    duration: 1500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+ScrollReveal().reveal('.AA1', {
+    duration: 2500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+ScrollReveal().reveal('.AA2', {
+    duration: 2000,
+    distance: '200px',
+    origin: 'right',
+    //scale: 1,
+    reset: true
+});
